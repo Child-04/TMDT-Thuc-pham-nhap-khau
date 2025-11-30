3f74d97679fd0c274c29b31f077be32644b16cdf
Nhhs1509 <nhhs1509@nhhs.local>
2025-11-30 19:05:34 +0700

    update userConstant

--- client/src/constants/UserConstant.js
+++ client/src/constants/UserConstant.js
@@ -1,4 +1,6 @@
 export const USER_LOGIN_REQUEST = 'USER_SIGNIN_REQUEST';
 export const USER_LOGIN_SUCCESS = 'USER_SIGNIN_SUCCESS';
 export const USER_LOGIN_FAIL = 'USER_SIGNIN_FAIL';
-export const BASE_URL = "http://localhost:4000"
+export const BASE_URL = process.env.REACT_APP_API_URL || process.env.NODE_ENV === 'production' 
+  ? "https://tpnk-server.onrender.com" 
+  : "http://localhost:4000";
